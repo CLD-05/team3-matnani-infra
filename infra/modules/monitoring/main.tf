6d8b104b5b0f809dfe876ea8b02e81191d6becf5
WONJUN6 <cnfdornqrl20@gamil.com>
2026-06-08 15:59:10 +0900

    feat: Prometheus Grafana,   모듈 구성

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -1,11 +1,13 @@
 # modules/monitoring/main.tf
 
+# 1. prometheus & Grafana 배포
 resource "helm_release" "monitoring" {
   name       = "team3-matnani-monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
   version    = "60.0.0"
   namespace  = "monitoring"
+  create_namespace = true #네임스페이스 자동 생성 추가
 
   # 가이드라인: 배포 안정성 확보
   wait    = true
@@ -22,4 +24,53 @@ resource "helm_release" "monitoring" {
     name  = "prometheus.prometheusSpec.externalLabels.environment"
     value = var.environment
   }
+}
+# 2. 로그 수집 파이프라인 ( Cloudwatch )
+resource "aws_cloudwatch_log_group" "eks_logs" {
+  name = "/aws/eks/team3-matnani-${var.environment}/application"
+  retention_in_days = 3
+}
+
+resource "helm_release" "fluent_bit" {
+  name       = "aws-for-fluent-bit"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-for-fluent-bit"
+  namespace  = "kube-system"
+
+  set {
+    name  = "cloudWatchLogs.region"
+    value = "ap-northeast-2"
+  }
+  set {
+    name  = "cloudWatchLogs.logGroupName"
+    value = aws_cloudwatch_log_group.eks_logs.name
+  }
+}
+
+# 알람 파이프라인 ( SNS + Chatbot )
+resource "aws_sns_topic" "matnani_alerts" {
+  name = "matnani-monitoring-alerts-${var.environment}"
+}
+
+# AWS Chatbot 연동 (Slack)
+resource "aws_chatbot_slack_channel_configuration" "matnani_slack" {
+  configuration_name = "matnani-slack-alerts"
+  iam_role_arn       = aws_iam_role.chatbot_role.arn
+  slack_team_id      = var.slack_workspace_id
+  slack_channel_id   = var.slack_channel_id
+  sns_topic_arns     = [aws_sns_topic.matnani_alerts.arn]
+}
+
+
+# Chatbot을 위한 최소 권한 IAM Role
+resource "aws_iam_role" "chatbot_role" {
+  name = "matnani-chatbot-role-${var.environment}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "chatbot.amazonaws.com" }
+    }]
+  })
 }
--- infra/modules/monitoring/variables.tf
+++ infra/modules/monitoring/variables.tf
@@ -1,7 +1,18 @@
-# modules/monitoring/variable.tf
+# modules/monitoring/variables.tf
 
 variable "environment" {
   description = "배포 환경 (dev / prod)"
   type        = string
   default     = "dev"
+}
+
+# [추가] Slack 연동을 위한 변수
+variable "slack_workspace_id" {
+  description = "Slack 워크스페이스 ID"
+  type        = string
+}
+
+variable "slack_channel_id" {
+  description = "알림을 받을 Slack 채널 ID"
+  type        = string
 }
