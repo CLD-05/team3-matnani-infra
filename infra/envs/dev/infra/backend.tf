272cbb0ddb47e3007905b7e568553090e3487a03
dltkdgur02 <sanhana10@naver.com>
2026-06-09 09:19:43 +0900

    fix(tf): backend key team3/ 접두사 복구

--- infra/envs/dev/infra/backend.tf
+++ infra/envs/dev/infra/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket       = "tfstate-lionkdt5-team3"
-    key          = "dev/infra/terraform.tfstate"
+    key          = "team3/dev/infra/terraform.tfstate"
     region       = "ap-northeast-2"
     encrypt      = true
     use_lockfile = true
