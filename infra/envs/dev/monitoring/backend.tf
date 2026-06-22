0ea171d987f2778de4854b103ed9613b83aed971
dltkdgur02 <sanhana10@naver.com>
2026-06-22 15:51:18 +0900

    chore: monitoring env layer 제거 및 GHA 워크플로우 정리

--- infra/envs/dev/monitoring/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tfstate-lionkdt5-team3"
-    key          = "team3/dev/monitoring/terraform.tfstate"
-    region       = "ap-northeast-2"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
